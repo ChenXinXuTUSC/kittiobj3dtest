f89737752a4000e0f215e74385075d0be50b102e
chenxinxu <1031307782@qq.com>
2024-12-22 02:06:44 +0800

    feat: store all yaml conf that have been read in

--- core/conf/readconfyaml.py
+++ core/conf/readconfyaml.py
@@ -1,10 +1,14 @@
+import os.path as osp
 import yaml
 import easydict
 
+stored = easydict.EasyDict()
 
-def read(yml_path: str):
+def read(yaml_path: str):
     # 读取 YAML 文件
-    with open(yml_path, 'r', encoding='utf-8') as file:
+    with open(yaml_path, 'r', encoding='utf-8') as file:
         # 将 YAML 内容转换为字典
         data = yaml.safe_load(file)
-    return easydict.EasyDict(data)
+    conf_name, _ = osp.splitext(yaml_path)
+    stored[conf_name] = easydict.EasyDict(data)
+    return stored[conf_name]
