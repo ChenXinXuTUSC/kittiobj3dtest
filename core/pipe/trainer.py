868ed303de454f87ffacc55b390f185a252e0979
chenxinxu <1031307782@qq.com>
2025-01-03 22:15:17 +0800

    ckpt: trainer should not know detail of other modules

--- core/pipe/trainer.py
+++ core/pipe/trainer.py
@@ -5,8 +5,11 @@ sys.path.append("..")
 from datetime import datetime
 
 import torch
-import torch.nn as nn
 import torch.utils.data
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader, DistributedSampler
 from torch.utils.tensorboard import SummaryWriter
 
 from collections import defaultdict
@@ -14,69 +17,153 @@ from collections import defaultdict
 import utils
 
 class Trainer:
-    '''
-    - tfx_logdir: will add timestamp subdir
-    '''
     def __init__(
         self,
+        # distributed training args
+        device_mod: str,
+        world_size: int,
+        # model and data args
         model: torch.nn.Module,
-        train_dataloader: torch.utils.data.DataLoader,
-        valid_dataloader: torch.utils.data.DataLoader,
-        cls_weight: list, # may be None
-        ignore_cls: int, # don't ignore the background class
-        bkgcls_idx: int, # should be the background class index
+        train_dataset: torch.utils.data.Dataset,
+        valid_dataset: torch.utils.data.Dataset,
+        criterion: torch.nn.Module,
+        metric_fn: callable,
+        # training args
         num_epochs: int,
+        batch_size: int,
+        lr: float,
+        # logging args
         log_exname: str,
         log_alldir: str,
         log_interv: int = 10
     ):
         # model and data
+        assert device_mod == "cpu" or \
+               device_mod == "cuda", \
+               f"invalid device {device_mod}"
+        self.device_mod = device_mod
+        self.world_size = world_size
+
         self.model = model
-        self.train_dataloader = train_dataloader
-        self.valid_dataloader = valid_dataloader
-        self.cls_weight = cls_weight 
-        if cls_weight is not None:
-            self.cls_weight = torch.tensor(cls_weight, dtype=torch.float)
-        
-        self.ignore_cls = ignore_cls
-        self.bkgcls_idx = bkgcls_idx
+        self.train_dataset = train_dataset
+        self.valid_dataset = valid_dataset
+        self.criterion = criterion
+        self.metric_fn = metric_fn
+
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.lr = lr
 
         # log staff
         self.log_interv = log_interv
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        log_alldir = osp.join(log_alldir, log_exname, timestamp)
-        tfx_logdir = osp.join(log_alldir, "tfx")
-        if not osp.exists(tfx_logdir):
-            os.makedirs(tfx_logdir, exist_ok=True)
-        self.tfx_logger = SummaryWriter(log_dir=tfx_logdir)
-        self.txt_logger = utils.get_logger(
-            log_exname, True,
-            osp.join(log_alldir, "run.log")
-        )
-        self.ckpt_saved = osp.join(log_alldir, "ckpt")
-        if not osp.exists(self.ckpt_saved):
-            os.makedirs(self.ckpt_saved, exist_ok=True)
+        self.log_alldir = osp.join(log_alldir, log_exname, timestamp)
+        self.log_exname = log_exname
 
-        # misc
-        self.num_epochs = num_epochs
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.best_miou = -1.0
+    def ddp_setup(self, rank: int, world_size: int):
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '12355'
+
+        # Initialize the process group
+        backend = "nccl" if self.device_mod == "cuda" else "gloo"
+        dist.init_process_group(backend=backend, rank=rank, world_size=world_size)
+
+    def ddp_cleanup(self):
+        dist.destroy_process_group()
+
+    
+    def run(self):
+        mp.spawn(
+            fn=self.ddp_entry,
+            args=(self.world_size,),
+            nprocs=self.world_size,
+            join=True
+        )
+    
+    def ddp_entry(self, world_rank: int, world_size: int):
+        self.world_rank = world_rank
+        self.world_size = world_size
+
+        # =====================================================================
+        # ============================= ddp setup =============================
+        self.ddp_setup(world_rank, world_size)
+        # ============================= ddp setup =============================
+        # =====================================================================
+
+        self.tfx_logger = None
+        self.txt_logger = None
+        
+        if world_rank == 0:
+            tfx_logdir = osp.join(self.log_alldir, "tfx")
+            if not osp.exists(tfx_logdir):
+                os.makedirs(tfx_logdir, exist_ok=True)
+            self.tfx_logger = SummaryWriter(log_dir=tfx_logdir)
+            self.txt_logger = utils.get_logger(
+                self.log_exname, True,
+                osp.join(self.log_alldir, "run.log")
+            )
+            self.ckpt_saved = osp.join(self.log_alldir, "ckpt")
+            if not osp.exists(self.ckpt_saved):
+                os.makedirs(self.ckpt_saved, exist_ok=True)
+
+        self.device = torch.device(world_rank if self.device_mod == "cuda" else "cpu")
+
+        self.model = self.model.to(self.device)
+        self.model = DDP(self.model, device_ids=[world_rank] if self.device_mod == "cuda" else None)
+        self.criterion = self.criterion.to(self.device)
+
+        self.train_sampler = DistributedSampler(
+            self.train_dataset, num_replicas=world_size, rank=world_rank
+        )
+        self.train_dataloader = DataLoader(
+            dataset=self.train_dataset,
+            batch_size=self.batch_size,
+            sampler=self.train_sampler,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+        self.valid_sampler = DistributedSampler(
+            dataset=self.valid_dataset, num_replicas=world_size, rank=world_rank
+        )
+        self.valid_dataloader = DataLoader(
+            dataset=self.valid_dataset,
+            batch_size=self.batch_size,
+            sampler=self.valid_sampler,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+
+
+        # =====================================================================
+        # ============================ entry point ============================
+        if world_rank == 0:
+            self.txt_logger.info("start training...")
+            self.txt_logger.info(f"model: {self.model}")
+        self.train()
+        # ============================ entry point ============================
+        # =====================================================================
+
+
+
+        # =====================================================================
+        # ============================ ddp cleanup ============================
+        self.ddp_cleanup()
+        # ============================ ddp cleanup ============================
+        # =====================================================================
 
 
     def train(self):
+        train_sampler = self.train_sampler
+        train_dataloader = self.train_dataloader
         model = self.model
-        # if torch.cuda.device_count() > 1:
-        #     model = torch.nn.parallel.DataParallel(self.model)
-        model = model.to(self.device)
-        optimizer = torch.optim.Adam(model.parameters())
+        criterion = self.criterion
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-        criterion = torch.nn.CrossEntropyLoss(
-            weight=self.cls_weight,
-            ignore_index=self.ignore_cls
-        ).to(self.device)
 
-        self.txt_logger.info("start training...")
-        self.txt_logger.info(f"CELoss weight: {self.cls_weight}, ignore_cls: {self.ignore_cls}")
+        self.best_miou = 0.0
+
         model.train()
         for epoch in range(self.num_epochs):
             epoch_loss = 0.0
@@ -84,11 +171,13 @@ class Trainer:
             epoch_mrec = defaultdict(list)
             epoch_miou = defaultdict(list)
 
-            for batch_idx, (fmap, gdth) in enumerate(self.train_dataloader):
-                fmap = fmap.to(self.device).float()
+            train_sampler.set_epoch(epoch)
+            for batch_idx, (data, gdth) in enumerate(train_dataloader):
+                data = data.to(self.device).float()
                 gdth = gdth.to(self.device).long()
 
-                loss, pred = model(fmap, gdth, criterion)
+                pred = model(data)
+                loss = criterion(pred, gdth)
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -105,7 +194,7 @@ class Trainer:
                 for k, v in iou.items():
                     epoch_miou[k].append(v)
 
-                if (batch_idx + 1) % self.log_interv == 0:
+                if self.world_rank == 0 and (batch_idx + 1) % self.log_interv == 0:
                     self.log_metrics(
                         metric_dict={
                             "loss": loss,
@@ -118,21 +207,23 @@ class Trainer:
                         }, epoch=epoch, iter=batch_idx, to_tfb=True, tfx_tag="train"
                     )
 
-            epoch_loss /= len(self.train_dataloader)
-            for k, v in epoch_macc.items():
-                epoch_macc[k] = sum(v) / len(v)
-            for k, v in epoch_mrec.items():
-                epoch_mrec[k] = sum(v) / len(v)
-            for k, v in epoch_miou.items():
-                epoch_miou[k] = sum(v) / len(v)
-            self.log_metrics(
-                metric_dict={
-                    "epoch_mlss": epoch_loss,
-                    "epoch_miou": epoch_miou,
-                    "epoch_mrec": epoch_mrec,
-                    "epoch_macc": epoch_macc
-                }, epoch=epoch, iter=len(self.train_dataloader), tfx_tag="train"
-            )
+
+            if self.world_rank == 0:
+                epoch_loss /= len(train_dataloader)
+                for k, v in epoch_macc.items():
+                    epoch_macc[k] = sum(v) / len(v)
+                for k, v in epoch_mrec.items():
+                    epoch_mrec[k] = sum(v) / len(v)
+                for k, v in epoch_miou.items():
+                    epoch_miou[k] = sum(v) / len(v)
+                self.log_metrics(
+                    metric_dict={
+                        "epoch_mlss": epoch_loss,
+                        "epoch_miou": epoch_miou,
+                        "epoch_mrec": epoch_mrec,
+                        "epoch_macc": epoch_macc
+                    }, epoch=epoch, iter=len(self.train_dataloader), tfx_tag="train"
+                )
 
             self.valid(epoch)
 
@@ -140,11 +231,16 @@ class Trainer:
 
 
     def valid(self, epoch: int):
-        criterion = torch.nn.CrossEntropyLoss(
-            weight=self.cls_weight,
-            ignore_index=self.ignore_cls
-        ).to(self.device)
-        model = self.model.to(device=self.device)
+        valid_sampler = self.valid_sampler
+        valid_dataloader = DataLoader(
+            dataset=self.valid_dataset,
+            batch_size=self.batch_size,
+            sampler=valid_sampler,
+            num_workers=4,
+            pin_memory=True,
+        )
+        model = self.model
+
         model.eval()
         with torch.no_grad():
             valid_loss = 0.0
@@ -152,11 +248,12 @@ class Trainer:
             valid_mrec = defaultdict(list)
             valid_miou = defaultdict(list)
 
-            for batch_idx, (fmap, gdth) in enumerate(self.valid_dataloader):
-                fmap = fmap.to(device=self.device).float()
-                gdth = gdth.to(device=self.device).long()
+            valid_sampler.set_epoch(epoch)
+            for batch_idx, (fmap, gdth) in enumerate(valid_dataloader):
+                fmap = fmap.to(self.device).float()
+                gdth = gdth.to(self.device).long()
 
-                loss, pred = model(fmap, gdth, criterion)
+                loss, pred = model(fmap, gdth)
 
                 loss = loss.item()
                 valid_loss += loss
@@ -180,7 +277,7 @@ class Trainer:
                     )
                     self.log_images(fmap, pred, gdth, epoch, batch_idx, "valid")
             
-            valid_loss /= len(self.valid_dataloader)
+            valid_loss /= len(valid_dataloader)
             for k, v in valid_macc.items():
                 valid_macc[k] = sum(v) / len(v)
             for k, v in valid_mrec.items():
@@ -197,9 +294,9 @@ class Trainer:
             )
 
             curr_miou = sum([v for v in valid_miou.values()]) / len(valid_miou)
-            if curr_miou > self.best_macc:
+            if curr_miou > best_miou:
                 self.txt_logger.info(f"best model saved with macc: {curr_miou:.3f}")
-                self.best_macc = curr_miou
+                best_miou = curr_miou
                 torch.save(self.model.state_dict(), osp.join(self.ckpt_saved, "best.pth"))
     
 
