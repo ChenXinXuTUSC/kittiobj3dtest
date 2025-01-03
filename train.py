937e4007e63bb4e5b422c699e1a893e8e6d7b50d
chenxinxu <1031307782@qq.com>
2025-01-03 22:16:40 +0800

    ckpt: read main conf and derive sub conf

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import os
+
+import core.metric
 os.environ["TORCH_HOME"] = "."
 import os.path as osp
 import argparse
@@ -8,57 +10,49 @@ import torch.utils.data
 
 import core
 
-
 parser = argparse.ArgumentParser("train.py")
-args_group_conf = parser.add_argument_group("conf", "arguments related to conf read")
-args_group_conf.add_argument("--conf_datad_yaml", type=str, help="path to datad conf file")
-args_group_conf.add_argument("--conf_model_yaml", type=str, help="path to model conf file")
-args_group_conf.add_argument("--conf_train_yaml", type=str, help="path to train conf file")
+parser.add_argument("--conf", type=str, help="main configuration yaml file path")
 
 
 def main():
     args = parser.parse_args()
-    conf_datad = core.readconfyaml.read(args.conf_datad_yaml)
-    conf_model = core.readconfyaml.read(args.conf_model_yaml)
-    conf_train = core.readconfyaml.read(args.conf_train_yaml)
+    conf_main = core.readconfyaml.read(args.conf)
+    
+    args_dataset = core.readconfyaml.read(conf_main.dataset.conf_yaml)
+    args_model   = core.readconfyaml.read(conf_main.model.conf_yaml)
+    args_loss    = core.readconfyaml.read(conf_main.loss.conf_yaml)
+    args_metric  = core.readconfyaml.read(conf_main.metric.conf_yaml)
 
-    train_dataloader = torch.utils.data.DataLoader(
-        dataset=core.dataset.DATASET[conf_train.dataset](
-            conf_datad.root, "train", conf_datad
-        ),
-        batch_size=conf_train.batch_size,
-        shuffle=True,
-        num_workers=4
-    )
+    # dataset is more important than model
+    train_dataset=core.dataset.DATASET[conf_main.dataset.cls_name](**args_dataset, split="train")
+    valid_dataset=core.dataset.DATASET[conf_main.dataset.cls_name](**args_dataset, split="valid")
 
-    valid_dataloader = torch.utils.data.DataLoader(
-        dataset=core.dataset.DATASET[conf_train.dataset](
-            conf_datad.root, "valid", conf_datad
-        ),
-        batch_size=conf_train.batch_size,
-        shuffle=True,
-        num_workers=4
-    )
+    # create corresponding model
+    model = core.model.MODEL[conf_main.model.cls_name](**args_model)
 
-    model = core.model.MODEL[conf_train.model](
-        in_channels=conf_model.in_channels,
-        out_channels=conf_model.num_classes
+    criterion = core.loss.LOSS[conf_main.loss.cls_name](**args_loss)
+
+    metriclog = core.metric.METRIC[conf_main.metric.cls_name](
+        **args_metric,
+        log_dir=conf_main.log_alldir,
+        log_exname=conf_main.exp_name
     )
 
     trainer = core.pipe.Trainer(
+        device_mod="cuda" if torch.cuda.is_available() else "cpu",
+        world_size=torch.cuda.device_count() if torch.cuda.is_available() else os.cpu_count(),
         model=model,
-        train_dataloader=train_dataloader,
-        valid_dataloader=valid_dataloader,
-        cls_weight=conf_train.cls_weight,
-        ignore_cls=conf_train.ignore_cls,
-        bkgcls_idx=conf_train.bkgcls_idx,
-        num_epochs=conf_train.num_epochs,
-        log_exname=conf_train.exp_name,
-        log_alldir=conf_train.log_alldir,
-        log_interv=conf_train.log_interv
+        train_dataset=train_dataset,
+        valid_dataset=valid_dataset,
+        num_epochs=conf_main.num_epochs,
+        batch_size=conf_main.batch_size,
+        lr=conf_main.lr,
+        log_exname=conf_main.exp_name,
+        log_alldir=conf_main.log_alldir,
+        log_interv=conf_main.log_interv
     )
 
-    trainer.train()
+    trainer.run()
 
 
 if __name__ == "__main__":
