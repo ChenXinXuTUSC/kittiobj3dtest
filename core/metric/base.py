85590951e874fd73ed90f11a3128a57103e43ce9
chenxinxu <1031307782@qq.com>
2025-04-27 15:35:18 +0800

    fix: log output change to stdout

--- core/metric/base.py
+++ core/metric/base.py
@@ -2,14 +2,16 @@ class Metric:
     def __init__(self):
         pass
 
-    def best(self):
-        pass
+    def best(self) -> bool:
+        # best 应该返回根据目前积累的指标数据计算出的指标
+        # 是否优于之前计算过的最好指标
+        raise NotImplementedError("metric best not defined")
 
     def mean(self):
-        pass
+        raise NotImplementedError("metric mean not defined")
 
     def reset(self):
-        pass
+        raise NotImplementedError("metric reset not defined")
     
     def mct(self,
         data, pred, gdth
