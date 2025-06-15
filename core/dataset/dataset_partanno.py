9d48e5d15ff76e481764190d239a159c66372386
chenxinxu <1031307782@qq.com>
2025-06-16 00:59:41 +0800

    ckpt: partanno lab.

--- core/dataset/dataset_partanno.py
+++ core/dataset/dataset_partanno.py
@@ -24,18 +24,41 @@ class PartAnno(BaseDataset):
 		self.args = kwds
 
 		self.split = self.args.split
+		assert self.split in self.args.seqs_split, \
+			f"{self.split} not in dataset conf"
 
-		self.split = self.args.split
-		assert self.split in self.args.seqs_split, f"{self.split} not in dataset conf"
+		self.selecte_cls_name = self.args.selected_cls_name
+		assert self.selecte_cls_name in self.args.cls_names, \
+			f"{self.selecte_cls_name} not in dataset conf"
 
-		self.root = osp.join(self.args.root, 'sequences')
-
-		self.data_dir = osp.join('partanno')
-		self.file_list = sorted(os.listdir(self.data_dir))
+		self.points_root = osp.join(self.args.root, self.selecte_cls_name, "points")
+		assert osp.exists(self.points_root), \
+			f"{self.root} points not exists"
+		self.labels_root = osp.join(self.args.root, self.selecte_cls_name, "points_label")
+		assert osp.exists(self.labels_root), \
+			f"{self.root} labels not exists"
+		
+		assert sum(self.args.seqs_split.values()) <= 1.0, \
+			f"sum of seqs_split should be <= 1.0"
+		
+		self.file_list = sorted(os.listdir(self.root))
 		np.random.shuffle(self.file_list)
 
-		train_size = int(len(self.file_list) * self.args.split)
-		self.train_files = self.file_list[:train_size]
-		self.val_files = self.file_list[train_size:]
+		# 根据每个 split 的比例，划分数据集
+		self.split_files = {}
+		start = 0
+		for split, ratio in self.args.seqs_split.items():
+			end = start + int(len(self.file_list) * ratio)
+			self.split_files[split] = self.file_list[start:end]
+			start = end
+			if end >= len(self.file_list):
+				break
+		
+		self.files = self.split_files[self.split]
+		for i in range(len(self.files)):
+			self.files[i] = osp.join(self.root, self.files)
 
-
+	def __getitem__(self, index):
+		# return super().__getitem__(index)
+		points = np.loadtxt(osp.join(PARTNO_ROOT, synset, "points", f"{sample}.pts"), delimiter=" ")
+		labels = np.loadtxt(osp.join(PARTNO_ROOT, synset, "points_label", f"{sample}.seg"), delimiter=" ").astype(np.int32)
--- core/model/model_minitransunet.py
+++ core/model/model_minitransunet.py
@@ -61,12 +61,20 @@ class TransUNetMini(nn.Module):
 		self.up_channels = kwds.up_channels # channels during upsample
 		assert len(self.dn_channels) == len(self.up_channels), \
 			"UNet structure requires same number of up and down sample"
+		assert self.dn_channels[-1] == self.up_channels[0], \
+			"UNet structure requires same feature channels between downsample and upsample end point"
 
+		self.dn_channels = [kwds.in_channels] + self.dn_channels
+		self.up_channels = self.up_channels + [kwds.out_channels]
+
+		self.dn_convs = [ConvDn(self.dn_channels[i], self.dn_channels[i+1]) for i in range(len(self.dn_channels)-1)]
+		self.up_convs = [ConvUp(self.up_channels[i], self.up_channels[i+1]) for i in range(len(self.up_channels)-1)]
 
 		# 注意图块嵌入器的图像大小是降采样最后输出的尺寸，不是图像原始尺寸
+		# 将一幅特征图转换为一个 patch-token 序列
 		self.embeder = Embedding(
 			kwds.embd_image_shape,
-			kwds.embd_patch_shape,
+			(1, 1),
 			kwds.in_channels,
 			kwds.out_channels
 		)
@@ -80,6 +88,32 @@ class TransUNetMini(nn.Module):
 		self.seg_head = None # todo: finish
 	
 	def forward(self, x: torch.Tensor):
-		# 进入的样本批次形状为 [B, N, H, W, C]，压缩为 [B*N, H, W, C]
 		# 一个样本具有多个视角投影图，现在并行卷积，不区分每个特征图具体来自哪个样本
-		x = x.view(-1, *x.shape[2:])
+		# 进入的样本批次形状为 [B, N, H, W, C]，压缩为 [B*N, H, W, C]
+		B, I, C, H, W = x.size()
+		x = x.view(B * I, C, H, W)
+
+		# 两层下采样
+		dn_out_list = []
+		for dn_conv in self.dn_convs:
+			dn_out_list.append(dn_conv(x))
+		_, _, CONV_DN_H, CONV_DN_W = x.size()
+
+		# Transformer 编码器
+		x = self.encoder(x)
+
+		# 编码器输出重新转换为上采样所需维度
+		_, S, E = x.size() # [忽略样本批次，序列长度，嵌入维度]
+		x = x.view(B, I, S, E) # [样本批次，每个样本视图数量，每个视图的序列长度，嵌入维度]
+		# 交换最后两个维度
+		x = x.permute(0, 1, 3, 2)
+		x = x.view(B, I, E, CONV_DN_H, CONV_DN_W)
+
+		# 两层上采样
+		up_out_list = []
+		for up_conv in self.up_convs:
+			up_out_list.append(up_conv(x))
+		
+		# 最终输出
+
+
